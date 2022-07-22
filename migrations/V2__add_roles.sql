259fb8b21b54b558f24ca0e5d9a21cdde6db7fde
Thomas Michels Rodrigues <thomasmichels.bnu@gmail.com>
2022-07-21 21:31:24 -0300

    feat: Added flyway migrations

---
+++ migrations/V2__add_roles.sql
@@ -0,0 +1,4 @@
+insert into roles(role_id, role_name) values (1, 'Update');
+insert into roles(role_id, role_name) values (2, 'Read');
+insert into roles(role_id, role_name) values (3, 'Write');
+insert into roles(role_id, role_name) values (4, 'Delete');
