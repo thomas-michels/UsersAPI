ed247f881e7c88a6cd60cdb4f14404f18d1e9b10
Thomas Michels Rodrigues <thomasmichels.bnu@gmail.com>
2022-07-24 23:30:09 -0300

    feat: Added CORS middleware

--- app/application.py
+++ app/application.py
@@ -2,6 +2,7 @@
     Application file
 """
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class FastAPIApplication:
@@ -12,5 +13,13 @@ class FastAPIApplication:
     @staticmethod
     def get_application():
         app = FastAPI()
+
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
         return app
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,3 +2,4 @@
     Module for DB
 """
 from app.db.postgres_repository import PostgresRepository
+from app.db.idb_repository import IDBRepository
