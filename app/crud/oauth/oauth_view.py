44876c92a6b8df504316e0da6ad2a35fc2cea636
Thomas Michels Rodrigues <thomasmichels.bnu@gmail.com>
2022-08-03 00:51:56 -0300

    refactor: Refactoring auth

---
+++ app/crud/oauth/oauth_view.py
@@ -0,0 +1,31 @@
+"""
+    Module for oauth
+"""
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from app.crud.users import UsersService, UsersRepository
+from app.db import PostgresRepository
+from app.configs import get_environment
+from app.shared_schemas import Token
+from datetime import timedelta
+from app.utils import create_access_token
+
+_env = get_environment()
+
+oauth_router = APIRouter(prefix="/oauth")
+repository = UsersRepository(connection=PostgresRepository())
+services = UsersService(repository=repository)
+
+
+@oauth_router.post("/token", response_model=Token, tags=["oauth"])
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = services.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    access_token_expires = timedelta(minutes=_env.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username, "scopes": form_data.scopes},
+        expires_delta=access_token_expires,
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/crud/users/users_view.py
+++ app/crud/users/users_view.py
@@ -2,103 +2,32 @@
     Module for Users Routes
 """
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, Security, status
-from fastapi.security import (
-    OAuth2PasswordBearer,
-    OAuth2PasswordRequestForm,
-    SecurityScopes,
-)
+from fastapi import APIRouter, Security
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-from app.crud.users import User, UserPublic, UsersService, UsersRepository, SimpleUser
+from app.crud.users import (
+    User,
+    UserPublic,
+    UsersService,
+    UsersRepository,
+    SimpleUser,
+    UserDB,
+)
+from app.crud.oauth import get_current_user
 from app.db import PostgresRepository
-from jose import JWTError, jwt
-from app.configs import get_environment
-from app.shared_schemas import Token, TokenData
-from pydantic import ValidationError
-from datetime import timedelta, datetime
-from app.utils import create_access_token
-
-_env = get_environment()
 
 
 users_router = APIRouter()
-
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="token",
-    scopes={"me": "Read information about the current user.", "items": "Read items."},
-)
 
 repository = UsersRepository(connection=PostgresRepository())
 services = UsersService(repository=repository)
 
 
-async def get_current_user(
-    security_scopes: SecurityScopes, token: str = Depends(oauth2_scheme)
-):
-    if security_scopes.scopes:
-        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
-
-    else:
-        authenticate_value = f"Bearer"
-
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": authenticate_value},
-    )
-
-    try:
-        payload = jwt.decode(token, _env.SECRET_KEY, algorithms=[_env.ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-
-        token_scopes = payload.get("scopes", [])
-        token_data = TokenData(scopes=token_scopes, username=username)
-
-    except (JWTError, ValidationError):
-        raise credentials_exception
-
-    user = repository.get_by_name_private(username=token_data.username)
-
-    if not user:
-        raise credentials_exception
-
-    for scope in security_scopes.scopes:
-        if scope not in token_data.scopes:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Not enough permissions",
-                headers={"WWW-Authenticate": authenticate_value},
-            )
-    return user
-
-
-async def get_current_active_user(
-    current_user: User = Security(get_current_user, scopes=["me"])
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@users_router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = services.authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    access_token_expires = timedelta(minutes=_env.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username, "scopes": form_data.scopes},
-        expires_delta=access_token_expires,
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
 @users_router.post("/users/", tags=["users"], response_model=UserPublic)
-def create_users(user: SimpleUser):
+def create_users(
+    user: SimpleUser,
+    current_user: UserDB = Security(get_current_user, scopes=["create"]),
+):
     """
     Route to create new user
     """
@@ -123,14 +52,14 @@ def create_users(user: SimpleUser):
 
 
 @users_router.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return JSONResponse(
-        status_code=200, content=jsonable_encoder(current_user.dict())
-    )
+async def read_users_me(
+    current_user: UserDB = Security(get_current_user, scopes=["me"])
+):
+    return JSONResponse(status_code=200, content=jsonable_encoder(current_user.dict()))
 
 
 @users_router.get("/users/", tags=["users"], response_model=List[UserPublic])
-def get_users():
+def get_users(current_user: UserDB = Security(get_current_user, scopes=["read"])):
     """
     Route to get user
     """
@@ -155,7 +84,9 @@ def get_users():
 
 
 @users_router.get("/users/{user_id}", tags=["users"], response_model=UserPublic)
-def get_users_by_id(user_id: str):
+def get_users_by_id(
+    user_id: str, current_user: UserDB = Security(get_current_user, scopes=["read"])
+):
     """
     Route to get user by id
     """
@@ -180,7 +111,11 @@ def get_users_by_id(user_id: str):
 
 
 @users_router.put("/users/{user_id}", tags=["users"], response_model=UserPublic)
-def update_users(user_id: str, user: SimpleUser):
+def update_users(
+    user_id: str,
+    user: SimpleUser,
+    current_user: UserDB = Security(get_current_user, scopes=["update"]),
+):
     """
     Route to update user
     """
@@ -201,7 +136,9 @@ def update_users(user_id: str, user: SimpleUser):
 
 
 @users_router.delete("/users/{user_id}", tags=["users"], response_model=UserPublic)
-def delete_users(user_id: str):
+def delete_users(
+    user_id: str, current_user: UserDB = Security(get_current_user, scopes=["delete"])
+):
     """
     Route to delete user
     """
@@ -219,10 +156,3 @@ def delete_users(user_id: str):
     return JSONResponse(
         status_code=response["status"], content=jsonable_encoder(response)
     )
-
-
-@users_router.get("/users/me/items/")
-async def read_own_items(
-    current_user: User = Security(get_current_active_user, scopes=["items"])
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
